52a3f4f64c504b62e3d26272ea33b9bba98c9884
Nishkarsh123 <nishkars.pal.3511@gmail.com>
2021-05-26 15:07:49 +0530

    Add files via upload

---
+++ nishkarsh.js
@@ -0,0 +1,15 @@
+function check()
+{
+  var name=document.getElementById('name').value;
+  var password=document.getElementById('password1').value;
+  // this is the comment
+  if(name==="Nishkarsh" && password==="Rajender@123")
+  alert("successfull");
+}
+
+function add()
+{
+  var a=document.getElementById('number1').value;
+  var b=document.getElementById('number2').value;
+ alert(Number(a)+Number(b));
+}
